e3aa6b40e80e54b39f5806dcaee291b79ee6ca4a
changerlemond <dev.jungclaire@gmail.com>
2023-10-15 20:15:10 +0900

    solve difference between two numbers

---
+++ src/main/java/programmers/basic/DifferenceBetweenTwoNumbers.java
@@ -0,0 +1,14 @@
+package programmers.basic;
+
+public class DifferenceBetweenTwoNumbers {
+
+    public static void main(String[] args) {
+        System.out.println(solution(1, 2));
+        System.out.println(solution(1, 1));
+    }
+
+    public static int solution(int num1, int num2) {
+        return num1 - num2;
+    }
+
+}
---
+++ src/main/kotlin/programmers/basic/DifferenceBetweenTwoNumbersKotlin.kt
@@ -0,0 +1,14 @@
+package programmers.basic
+
+class DifferenceBetweenTwoNumbersKotlin {
+
+    fun solution(num1: Int, num2: Int): Int {
+        return num1 - num2
+    }
+
+}
+
+fun main() {
+    println(DifferenceBetweenTwoNumbersKotlin().solution(1, 2))
+    print(DifferenceBetweenTwoNumbersKotlin().solution(5, 3))
+}
