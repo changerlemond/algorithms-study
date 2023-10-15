83161660ae267454fa4b285c0816c557ad9a55fe
changerlemond <dev.jungclaire@gmail.com>
2023-10-15 20:18:43 +0900

    solve multiplication of two numbers

---
+++ src/main/kotlin/programmers/basic/MultiplicationOfTwoNumbersKotlin.kt
@@ -0,0 +1,14 @@
+package programmers.basic
+
+class MultiplicationOfTwoNumbersKotlin {
+
+    fun solution(num1: Int, num2: Int): Int {
+        return num1 * num2
+    }
+
+}
+
+fun main() {
+    println(MultiplicationOfTwoNumbersKotlin().solution(1, 2))
+    print(MultiplicationOfTwoNumbersKotlin().solution(3, 4))
+}
