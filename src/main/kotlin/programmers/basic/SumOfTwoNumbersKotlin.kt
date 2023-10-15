3d1c8e67fbf26db61308636bd516ce1f4826ede0
changerlemond <dev.jungclaire@gmail.com>
2023-10-15 20:11:01 +0900

    solve sum of two numbers

---
+++ src/main/kotlin/programmers/basic/SumOfTwoNumbersKotlin.kt
@@ -0,0 +1,14 @@
+package programmers.basic
+
+class SumOfTwoNumbersKotlin {
+
+    fun solution(num1: Int, num2: Int): Int {
+        return num1 + num2
+    }
+
+}
+
+fun main() {
+    println(SumOfTwoNumbersKotlin().solution(1, 2))
+    print(SumOfTwoNumbersKotlin().solution(1, 3))
+}
