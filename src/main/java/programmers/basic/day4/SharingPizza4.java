8a4c7d818b41ea6906940d2c0df03fbef5d853da
changerlemond <dev.jungclaire@gmail.com>
2023-11-03 16:08:46 +0900

    solve sharing pizza (4)

---
+++ src/main/java/programmers/basic/day4/SharingPizza4.java
@@ -0,0 +1,15 @@
+package programmers.basic.day4;
+
+import java.util.Arrays;
+
+public class SharingPizza4 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+        System.out.println(solution(new int[]{89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99}));
+    }
+
+    public static double solution(int[] numbers) {
+        return Arrays.stream(numbers).average().orElse(0);
+    }
+}
