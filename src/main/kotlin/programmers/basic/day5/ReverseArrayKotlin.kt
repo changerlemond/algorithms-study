330c24ee7a566c109b770697d69fe6254020f6df
changerlemond <dev.jungclaire@gmail.com>
2023-11-03 16:59:15 +0900

    solve reverse array

---
+++ src/main/kotlin/programmers/basic/day5/ReverseArrayKotlin.kt
@@ -0,0 +1,15 @@
+package programmers.basic.day5
+
+class ReverseArrayKotlin {
+
+    fun solution(num_list: IntArray): IntArray {
+        return num_list.reversedArray()
+    }
+
+}
+
+fun main() {
+    println(ReverseArrayKotlin().solution(intArrayOf(1, 2, 3, 4, 5)).contentToString())
+    println(ReverseArrayKotlin().solution(intArrayOf(1, 1, 1, 1, 1, 2)).contentToString())
+    println(ReverseArrayKotlin().solution(intArrayOf(1, 0, 1, 1, 1, 3, 5)).contentToString())
+}
