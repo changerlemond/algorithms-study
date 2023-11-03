bebb9364841302dd0011e069fabb75b510fcbef8
changerlemond <dev.jungclaire@gmail.com>
2023-11-03 16:42:51 +0900

    solve Ice americano

---
+++ src/main/java/programmers/basic/day5/IceAmericano.java
@@ -0,0 +1,16 @@
+package programmers.basic.day5;
+
+import java.util.Arrays;
+
+public class IceAmericano {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(5500)));
+        System.out.println(Arrays.toString(solution(15000)));
+    }
+
+    public static int[] solution(int money) {
+        return new int[]{money / 5500, money % 5500};
+    }
+
+}
