2f0d6e36c942b3dd59b745e121cf9771afc82840
changerlemond <dev.jungclaire@gmail.com>
2023-11-08 00:05:08 +0900

    solve protractor

---
+++ src/main/kotlin/programmers/basic/day7/ProtractorKotlin.kt
@@ -0,0 +1,18 @@
+package programmers.basic.day7
+
+class ProtractorKotlin {
+
+    fun solution(angle: Int) = (when {
+        angle == 180 -> 4
+        angle > 90 -> 3
+        angle == 90 -> 2
+        else -> 1
+    }.toInt())
+
+}
+
+fun main() {
+    println(ProtractorKotlin().solution(70))
+    println(ProtractorKotlin().solution(91))
+    println(ProtractorKotlin().solution(180))
+}
