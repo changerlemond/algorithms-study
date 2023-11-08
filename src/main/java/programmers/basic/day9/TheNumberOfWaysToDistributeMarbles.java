7898eb5108c35d743a5886f6fb470e73878071cb
changerlemond <dev.jungclaire@gmail.com>
2023-11-08 17:02:14 +0900

    solve the number of ways to distribute marbles

---
+++ src/main/java/programmers/basic/day9/TheNumberOfWaysToDistributeMarbles.java
@@ -0,0 +1,24 @@
+package programmers.basic.day9;
+
+import java.math.BigInteger;
+
+public class TheNumberOfWaysToDistributeMarbles {
+
+    public static void main(String[] args) {
+        System.out.println(solution(3, 2));
+        System.out.println(solution(5, 3));
+    }
+
+    public static int solution(int balls, int share) {
+        BigInteger result = BigInteger.ONE;
+        int min = Math.min(share, balls - share);
+
+        for (int i = 0; i < min; i++) {
+            result = result.multiply(BigInteger.valueOf(balls - i));
+            result = result.divide(BigInteger.valueOf(i + 1));
+        }
+
+        return result.intValue();
+    }
+
+}
