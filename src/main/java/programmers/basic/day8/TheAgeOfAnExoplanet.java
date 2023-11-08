6ebcd2af0bbd263ba1d30e9a029e3c5e76cc0c75
changerlemond <dev.jungclaire@gmail.com>
2023-11-08 14:34:28 +0900

    solve the age of an exoplanet

---
+++ src/main/java/programmers/basic/day8/TheAgeOfAnExoplanet.java
@@ -0,0 +1,25 @@
+package programmers.basic.day8;
+
+public class TheAgeOfAnExoplanet {
+
+    public static void main(String[] args) {
+        System.out.println(solution(23));
+        System.out.println(solution(51));
+        System.out.println(solution(100));
+    }
+
+    public static String solution(int age) {
+        char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+
+        String numberString = Integer.toString(age);
+        StringBuilder result = new StringBuilder();
+
+        for (char digit: numberString.toCharArray()) {
+            int index = digit - '0';
+            result.append(alphabet[index]);
+        }
+
+        return result.toString();
+    }
+
+}
---
+++ src/main/kotlin/programmers/basic/day8/TheAgeOfAnExoplanetKotlin.kt
@@ -0,0 +1,25 @@
+package programmers.basic.day8
+
+class TheAgeOfAnExoplanetKotlin {
+
+    fun solution(age: Int): String {
+        val alphabet = charArrayOf('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j')
+
+        val numberString = age.toString()
+        val result = StringBuilder()
+
+        for (digit in numberString.toCharArray()) {
+            val index = digit.code - '0'.code
+            result.append(alphabet[index])
+        }
+
+        return result.toString()
+    }
+
+}
+
+fun main() {
+    println(TheAgeOfAnExoplanetKotlin().solution(23))
+    println(TheAgeOfAnExoplanetKotlin().solution(51))
+    println(TheAgeOfAnExoplanetKotlin().solution(100))
+}
