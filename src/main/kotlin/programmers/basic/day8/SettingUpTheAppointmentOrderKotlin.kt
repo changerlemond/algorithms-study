22a1c4b8d553960a6a1c32e5a44649ee099c8f1b
changerlemond <dev.jungclaire@gmail.com>
2023-11-08 15:09:38 +0900

    solve setting up the appointment order

---
+++ src/main/kotlin/programmers/basic/day8/SettingUpTheAppointmentOrderKotlin.kt
@@ -0,0 +1,25 @@
+package programmers.basic.day8
+
+class SettingUpTheAppointmentOrderKotlin {
+
+    fun solution(emergency: IntArray): IntArray {
+        val intArray = IntArray(emergency.size)
+        for (i in emergency.indices) {
+            var rank = 1
+            for (k in emergency) {
+                if (k > emergency[i]) {
+                    rank++
+                }
+            }
+            intArray[i] = rank
+        }
+        return intArray
+    }
+
+}
+
+fun main() {
+    println(SettingUpTheAppointmentOrderKotlin().solution(intArrayOf(3, 76, 24)).contentToString())
+    println(SettingUpTheAppointmentOrderKotlin().solution(intArrayOf(1, 2, 3, 4, 5, 6, 7)).contentToString())
+    println(SettingUpTheAppointmentOrderKotlin().solution(intArrayOf(30, 10, 23, 6, 100)).contentToString())
+}
