16a01faab57a15b217b5f8fa84ba20ef7c7da2d0
changerlemond <dev.jungclaire@gmail.com>
2023-11-08 15:22:02 +0900

    solve number of ordered pairs

---
+++ src/main/java/programmers/basic/day8/NumberOfOrderedPairs.java
@@ -0,0 +1,20 @@
+package programmers.basic.day8;
+
+public class NumberOfOrderedPairs {
+
+    public static void main(String[] args) {
+        System.out.println(solution(20));
+        System.out.println(solution(100));
+    }
+
+    public static int solution(int n) {
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+}
