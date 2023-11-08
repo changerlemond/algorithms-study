f4a0f1d15c6b3033edb4a466909ea71765e49e89
changerlemond <dev.jungclaire@gmail.com>
2023-11-08 17:34:53 +0900

    solve make it two-dimensional

---
+++ src/main/kotlin/programmers/basic/day10/MakeItTwoDimensionalKotlin.kt
@@ -0,0 +1,12 @@
+package programmers.basic.day10
+
+class MakeItTwoDimensionalKotlin {
+
+    fun solution(num_list: IntArray, n: Int) = num_list.toList().chunked(n)
+
+}
+
+fun main() {
+    println(MakeItTwoDimensionalKotlin().solution(intArrayOf(1, 2, 3, 4, 5, 6, 7, 8), 2))
+    println(MakeItTwoDimensionalKotlin().solution(intArrayOf(100, 95, 2, 4, 5, 6, 18, 33, 948), 3))
+}
