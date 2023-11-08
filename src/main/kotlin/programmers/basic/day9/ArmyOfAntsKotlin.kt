62e0d7c4a4cdb9abf217592ca5968149bb123da7
changerlemond <dev.jungclaire@gmail.com>
2023-11-08 15:40:47 +0900

    solve army of ants

---
+++ src/main/kotlin/programmers/basic/day9/ArmyOfAntsKotlin.kt
@@ -0,0 +1,26 @@
+package programmers.basic.day9;
+
+class ArmyOfAntsKotlin {
+
+    fun solution(hp: Int): Int {
+        var copyHp = hp
+        var count = 0
+
+        count += copyHp / 5
+        copyHp %= 5
+
+        count += copyHp / 3
+        copyHp %= 3
+
+        count += copyHp
+
+        return count
+    }
+
+}
+
+fun main() {
+    println(ArmyOfAntsKotlin().solution(23))
+    println(ArmyOfAntsKotlin().solution(24))
+    println(ArmyOfAntsKotlin().solution(999))
+}
