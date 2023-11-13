858e4fa7f831b6eef7b5194f77f0f2a0ef30eed4
changerlemond <dev.jungclaire@gmail.com>
2023-11-13 16:44:09 +0900

    solve number of dice

---
+++ src/main/kotlin/programmers/basic/day11/NumberOfDiceKotlin.kt
@@ -0,0 +1,14 @@
+package programmers.basic.day11
+
+class NumberOfDiceKotlin {
+
+    fun solution(box: IntArray, n: Int): Int {
+        return (box[0] / n) * (box[1] / n) * (box[2] / n)
+    }
+
+}
+
+fun main() {
+    println(NumberOfDiceKotlin().solution(intArrayOf(1, 1, 1), 1))
+    println(NumberOfDiceKotlin().solution(intArrayOf(10, 8, 6), 3))
+}
