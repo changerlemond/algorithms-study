b3120402cdb7c14da813c5ae25138ca769474cbd
changerlemond <dev.jungclaire@gmail.com>
2023-11-13 17:29:41 +0900

    solve factorial - tailrec

--- src/main/kotlin/programmers/basic/day11/FactorialKotlin.kt
+++ src/main/kotlin/programmers/basic/day11/FactorialKotlin.kt
@@ -1,5 +1,7 @@
 package programmers.basic.day11
 
+import java.util.concurrent.atomic.LongAccumulator
+
 class FactorialKotlin {
 
     fun solution(n: Int): Int {
@@ -13,9 +15,18 @@ class FactorialKotlin {
         return number - 1
     }
 
+    fun solution1(n: Int)= (2..n).find { n < factorialFunction(it) }?.dec() ?: n
+
+    // tailrec 키워드에 대해서 알아둘 것
+    private tailrec fun factorialFunction(n: Int, accumulator: Int = 1): Int {
+        return if (n == 1) accumulator else factorialFunction(n - 1, n * accumulator)
+    }
+
 }
 
 fun main() {
     println(FactorialKotlin().solution(3628800))
     println(FactorialKotlin().solution(7))
+    println(FactorialKotlin().solution1(3628800))
+    println(FactorialKotlin().solution1(7))
 }
