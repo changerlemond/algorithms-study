8864b258d7fbd43a16a0275d721b7ba6b4ba8aff
changerlemond <dev.jungclaire@gmail.com>
2023-11-14 19:05:14 +0900

    solve remove duplicate characters

---
+++ src/main/java/programmers/level0/day13/RemoveDuplicateCharacters.java
@@ -0,0 +1,18 @@
+package programmers.level0.day13;
+
+public class RemoveDuplicateCharacters {
+
+    public static void main(String[] args) {
+        System.out.println(solution("people"));
+        System.out.println(solution("We are the world"));
+    }
+
+    public static String solution(String my_string) {
+        return my_string
+                .codePoints()
+                .distinct()
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
+
+}
---
+++ src/main/kotlin/programmers/level0/day13/RemoveDuplicateCharactersKotlin.kt
@@ -0,0 +1,12 @@
+package programmers.level0.day13
+
+class RemoveDuplicateCharactersKotlin {
+
+    fun solution(my_string: String) = my_string.toList().distinct().joinToString("")
+
+}
+
+fun main() {
+    println(RemoveDuplicateCharactersKotlin().solution("people"))
+    println(RemoveDuplicateCharactersKotlin().solution("We are the world"))
+}
