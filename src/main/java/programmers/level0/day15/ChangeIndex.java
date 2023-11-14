244cee1b7b4b88c7f68f454622c93633d48d5894
changerlemond <dev.jungclaire@gmail.com>
2023-11-14 20:41:39 +0900

    solve change index

---
+++ src/main/java/programmers/level0/day15/ChangeIndex.java
@@ -0,0 +1,20 @@
+package programmers.level0.day15;
+
+public class ChangeIndex {
+
+    public static void main(String[] args) {
+        System.out.println(solution("hello", 1, 2));
+        System.out.println(solution("I love you", 3, 6));
+    }
+
+    public static String solution(String my_string, int num1, int num2) {
+        StringBuilder result = new StringBuilder(my_string);
+        char num1String = my_string.charAt(num1);
+        char num2String = my_string.charAt(num2);
+
+        result.setCharAt(num1, num2String);
+        result.setCharAt(num2, num1String);
+        return result.toString();
+    }
+
+}
