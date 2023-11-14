7fddbbc04ee903649135c6a900edce8d62285bd7
changerlemond <dev.jungclaire@gmail.com>
2023-11-14 22:10:07 +0900

    solve letter

---
+++ src/main/java/programmers/level0/day16/Letter.java
@@ -0,0 +1,14 @@
+package programmers.level0.day16;
+
+public class Letter {
+
+    public static void main(String[] args) {
+        System.out.println(solution("happy birthday!"));
+        System.out.println(solution("I love you~"));
+    }
+
+    public static int solution(String message) {
+        return message.length() * 2;
+    }
+
+}
---
+++ src/main/kotlin/programmers/level0/day16/LetterKotlin.kt
@@ -0,0 +1,12 @@
+package programmers.level0.day16
+
+class LetterKotlin {
+
+    fun solution(message: String) = message.length * 2
+
+}
+
+fun main() {
+    println(LetterKotlin().solution("happy birthday!"))
+    println(LetterKotlin().solution("I love you~"))
+}
