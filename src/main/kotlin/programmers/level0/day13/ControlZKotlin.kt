2a9e659e40ecde3e7db7a6d48c1207d19ea3a200
changerlemond <dev.jungclaire@gmail.com>
2023-11-14 18:25:28 +0900

    solve control z

---
+++ src/main/kotlin/programmers/level0/day13/ControlZKotlin.kt
@@ -0,0 +1,34 @@
+package programmers.level0.day13
+
+import java.util.*
+
+class ControlZKotlin {
+
+    fun solution(s: String): Int {
+        var answer = 0
+        val stack = Stack<Int>()
+
+        for (i in s.split(" ".toRegex())) {
+            if (i == "Z") {
+                stack.pop()
+            } else {
+                stack.push(i.toInt())
+            }
+        }
+
+        for (i in stack) {
+            answer += i
+        }
+
+        return answer
+    }
+
+}
+
+fun main() {
+    println(ControlZKotlin().solution("1 2 Z 3"))
+    println(ControlZKotlin().solution("10 20 30 40"))
+    println(ControlZKotlin().solution("10 Z 20 Z 1"))
+    println(ControlZKotlin().solution("10 Z 20 Z"))
+    println(ControlZKotlin().solution("-1 -2 -3 Z"))
+}
