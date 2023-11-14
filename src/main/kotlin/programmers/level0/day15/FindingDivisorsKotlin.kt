c30787fb9b631e2bc42fa742001cf54a4d5e8373
changerlemond <dev.jungclaire@gmail.com>
2023-11-14 21:12:37 +0900

    solve finding divisors

---
+++ src/main/kotlin/programmers/level0/day15/FindingDivisorsKotlin.kt
@@ -0,0 +1,12 @@
+package programmers.level0.day15
+
+class FindingDivisorsKotlin {
+
+    fun solution(n: Int) = (1..n).filter { n % it == 0 }
+
+}
+
+fun main() {
+    println(FindingDivisorsKotlin().solution(24))
+    println(FindingDivisorsKotlin().solution(29))
+}
