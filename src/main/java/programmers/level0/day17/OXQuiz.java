71c01b41b7ac5c5f613164c8bde21cdd840ea40f
changerlemond <dev.jungclaire@gmail.com>
2023-11-15 18:40:37 +0900

    solve ox quiz

---
+++ src/main/java/programmers/level0/day17/OXQuiz.java
@@ -0,0 +1,21 @@
+package programmers.level0.day17;
+
+import java.util.Arrays;
+
+public class OXQuiz {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new String[]{"3 - 4 = -3", "5 + 6 = 11"})));
+        System.out.println(Arrays.toString(solution(new String[]{"19 - 6 = 13", "5 + 66 = 71", "5 - 15 = 63", "3 - 1 = 2"})));
+    }
+
+    public static String[] solution(String[] quiz) {
+        for(int i=0; i<quiz.length; i++){
+            String[] text = quiz[i].split(" ");
+            int result = Integer.parseInt(text[0]) + ( Integer.parseInt(text[2]) * ( text[1].equals("+") ? 1:-1) );
+            quiz[i] = result == Integer.parseInt(text[4])? "O": "X";
+        }
+        return quiz;
+    }
+
+}
