305e0c1ffedb75e5081b9ef90fdb0acffa65130e
changerlemond <dev.jungclaire@gmail.com>
2023-11-18 22:00:57 +0900

    solve taller person

---
+++ src/main/java/programmers/level0/day19/TallerPerson.java
@@ -0,0 +1,16 @@
+package programmers.level0.day19;
+
+import java.util.Arrays;
+
+public class TallerPerson {
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{149, 180, 192, 170}, 167));
+        System.out.println(solution(new int[]{180, 120, 140}, 190));
+    }
+
+    public static int solution(int[] array, int height) {
+        return (int) Arrays.stream(array).filter(number -> height < number).count();
+    }
+
+}
