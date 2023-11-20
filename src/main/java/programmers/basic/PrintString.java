3f2a0ade039089c2f9fec73b991c36182717e0a4
changerlemond <dev.jungclaire@gmail.com>
2023-11-20 20:01:33 +0900

    solve print string

---
+++ src/main/java/programmers/basic/PrintString.java
@@ -0,0 +1,17 @@
+package programmers.basic;
+
+import java.util.Scanner;
+
+public class PrintString {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.next();
+        int length = string.length();
+
+        if (length >= 1 && length <= 1_000_000) {
+            System.out.println(string);
+        }
+    }
+
+}
---
+++ src/main/kotlin/programmers/basic/PrintStringKotlin.kt
@@ -0,0 +1,5 @@
+package programmers.basic
+
+fun main() {
+    println(readln())
+}
