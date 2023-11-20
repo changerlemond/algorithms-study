a2f780d9b56a15b07dc99118f795d7aebd15149a
changerlemond <dev.jungclaire@gmail.com>
2023-11-20 18:59:35 +0900

    solve number of k

---
+++ src/main/kotlin/programmers/level0/day24/NumberOfKKotlin.kt
@@ -0,0 +1,13 @@
+package programmers.level0.day24
+
+class NumberOfKKotlin {
+
+    fun solution(i: Int, j: Int, k: Int)  = (i..j).joinToString("").count { it.digitToInt() == k }
+
+}
+
+fun main() {
+    println(NumberOfKKotlin().solution(1, 13, 1))
+    println(NumberOfKKotlin().solution(10, 50, 5))
+    println(NumberOfKKotlin().solution(3, 10, 2))
+}
