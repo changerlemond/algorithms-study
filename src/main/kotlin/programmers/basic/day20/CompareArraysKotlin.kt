446c53dbc99f9ae57f1fc9bcd9c981186dc8d1e6
changerlemond <dev.jungclaire@gmail.com>
2024-01-28 21:31:32 +0900

    solve compare arrays

--- src/main/kotlin/programmers/basic/day20/CompareArraysKotlin.kt
+++ src/main/kotlin/programmers/basic/day20/CompareArraysKotlin.kt
@@ -9,11 +9,11 @@ fun main() {
 class CompareArraysKotlin {
 
     fun solution(arr1: IntArray, arr2: IntArray): Int {
-        if (arr1.size < arr2.size) return -1
-        else if (arr1.size > arr2.size) return 1
-        else if (arr1.sum() < arr2.sum()) return -1
-        else if (arr1.sum() > arr2.sum()) return 1
-        else return 0
+        return if (arr1.size < arr2.size) -1
+        else if (arr1.size > arr2.size) 1
+        else if (arr1.sum() < arr2.sum()) -1
+        else if (arr1.sum() > arr2.sum()) 1
+        else 0
     }
 
 }
