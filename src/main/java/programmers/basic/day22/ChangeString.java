66b998712aba63dd25651af91845fdf329aecdba
changerlemond <dev.jungclaire@gmail.com>
2024-01-29 22:59:29 +0900

    solve change string

---
+++ src/main/java/programmers/basic/day22/ChangeString.java
@@ -0,0 +1,14 @@
+package programmers.basic.day22;
+
+public class ChangeString {
+
+    public static void main(String[] args) {
+        System.out.println(solution(123));
+        System.out.println(solution(2573));
+    }
+
+    public static String solution(int n) {
+        return String.valueOf(n);
+    }
+
+}
---
+++ src/main/kotlin/programmers/basic/day22/ChangeStringKotlin.kt
@@ -0,0 +1,12 @@
+package programmers.basic.day22
+
+fun main() {
+    println(ChangeStringKotlin().solution(123))
+    println(ChangeStringKotlin().solution(2573))
+}
+
+class ChangeStringKotlin {
+
+    fun solution(n: Int) = n.toString()
+
+}
